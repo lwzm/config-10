aa735308dd5d8b888100ea80061a756935d00723
felloak <felloak@gmail.com>
2015-04-24 21:16:37 +0800

    down

--- down.sh
+++ down.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 for i in $(seq 1 10); do
-    #scp r$i:/etc/sysconfig/network-scripts/ifcfg-em1 r$i-ifcfg-em1
-    #scp r$i:/etc/sysconfig/network-scripts/ifcfg-em2 r$i-ifcfg-em2
+    scp r$i:/etc/sysconfig/network-scripts/ifcfg-em1 r$i-ifcfg-em1
+    scp r$i:/etc/sysconfig/network-scripts/ifcfg-em2 r$i-ifcfg-em2
     scp r$i:/etc/sysconfig/network-scripts/ifcfg-lo0 r$i-ifcfg-lo0
-    #scp r$i:/etc/quagga/ospfd.conf r$i-ospfd.conf
+    scp r$i:/etc/quagga/ospfd.conf r$i-ospfd.conf
 done
