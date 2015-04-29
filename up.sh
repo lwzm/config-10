cbabf6558e1b5ad562b5947fba839f703df3c8bf
felloak <felloak@gmail.com>
2015-04-29 16:58:47 +0800

    rsyslog

--- up.sh
+++ up.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 for i in $(seq 1 10); do
     scp r$i-ifcfg-em1     rr$i:/etc/sysconfig/network-scripts/ifcfg-em1
     scp r$i-ifcfg-em2     rr$i:/etc/sysconfig/network-scripts/ifcfg-em2
@@ -10,5 +11,13 @@ for i in $(seq 1 10); do
     scp sysctl.conf       rr$i:/etc/sysctl.conf
     scp hosts             rr$i:/etc/hosts
 done
+
+exit
+
+
+for i in $(seq 1 6); do
+    scp r1-r6.local.conf  rr$i:/etc/rsyslog.d/
+done
+scp r7.hub.conf rr7:/etc/rsyslog.d/hub.conf
 
 exit
