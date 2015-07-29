61659f25f05f1618cc29b21987451dbfe8176887
felloak <felloak@gmail.com>
2015-07-29 16:30:35 +0800

    add hosts

--- up.sh
+++ up.sh
@@ -2,14 +2,12 @@
 
 for i in $(seq 1 10); do
     scp r$i-ifcfg-bond0   rr$i:/etc/sysconfig/network-scripts/ifcfg-bond0
-    scp r$i-ifcfg-lo0     rr$i:/etc/sysconfig/network-scripts/ifcfg-lo0
     scp r$i-ospfd.conf    rr$i:/etc/quagga/ospfd.conf
 
     scp ifcfg-em1         rr$i:/etc/sysconfig/network-scripts/ifcfg-em1
     scp ifcfg-em2         rr$i:/etc/sysconfig/network-scripts/ifcfg-em2
     scp zebra.conf        rr$i:/etc/quagga/zebra.conf
     scp bonding.conf      rr$i:/etc/modprobe.d/bonding.conf
-    scp rt_tables         rr$i:/etc/iproute2/rt_tables
     scp sysctl.conf       rr$i:/etc/sysctl.conf
     scp hosts             rr$i:/etc/hosts
     scp rc.local          rr$i:/etc/rc.local
@@ -20,6 +18,7 @@ exit
 
 
 for i in $(seq 1 6); do
+    scp r1-r6-ifcfg-lo0   rr$i:/etc/sysconfig/network-scripts/ifcfg-lo0
     scp r1-r6.local.conf  rr$i:/etc/rsyslog.d/
 done
 scp r7.hub.conf rr7:/etc/rsyslog.d/hub.conf
