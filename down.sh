d2027fe779cca197ab8f27d1142426e22706d0c6
felloak <felloak@gmail.com>
2015-08-12 15:59:47 +0800

    hosts name bb*

--- down.sh
+++ down.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 for i in $(seq 1 10); do
-    scp rr$i:/etc/sysconfig/network-scripts/ifcfg-bond0     ifcfg-bond0/r$i   
-    scp rr$i:/etc/quagga/ospfd.conf                         ospfd.conf/r$i    
-    scp rr$i:/etc/sysconfig/network-scripts/ifcfg-em4       ifcfg-em4/r$i     
+    scp bb$i:/etc/sysconfig/network-scripts/ifcfg-bond0     ifcfg-bond0/r$i   
+    scp bb$i:/etc/quagga/ospfd.conf                         ospfd.conf/r$i    
+    scp bb$i:/etc/sysconfig/network-scripts/ifcfg-em4       ifcfg-em4/r$i     
 done
 exit
--- up.sh
+++ up.sh
@@ -1,28 +1,38 @@
 #!/bin/sh
 
-for i in $(seq 1 10); do
-    scp ifcfg-bond0/r$i   rr$i:/etc/sysconfig/network-scripts/ifcfg-bond0
-    scp ospfd.conf/r$i    rr$i:/etc/quagga/ospfd.conf
-    scp ifcfg-em4/r$i     rr$i:/etc/sysconfig/network-scripts/ifcfg-em4
+for i in 1 3 4 5 6; do
+    scp r1-r6.local.conf  bb$i:/etc/rsyslog.d/
+done
+exit
 
-    scp ifcfg-em1         rr$i:/etc/sysconfig/network-scripts/ifcfg-em1
-    scp ifcfg-em2         rr$i:/etc/sysconfig/network-scripts/ifcfg-em2
-    scp zebra.conf        rr$i:/etc/quagga/zebra.conf
-    scp bonding.conf      rr$i:/etc/modprobe.d/bonding.conf
-    scp sysctl.conf       rr$i:/etc/sysctl.conf
-    scp hosts             rr$i:/etc/hosts
-    scp rc.local          rr$i:/etc/rc.local
-    scp nginx.conf        rr$i:/etc/nginx
-    scp dstat.sh          r$i:bin/
+for i in 1 3 4 5 6 7 9 10; do
+    scp hosts             bb$i:/etc/hosts
+done
+exit
+
+for i in $(seq 1 10); do
+    scp ifcfg-bond0/r$i   bb$i:/etc/sysconfig/network-scripts/ifcfg-bond0
+    scp ospfd.conf/r$i    bb$i:/etc/quagga/ospfd.conf
+    scp ifcfg-em4/r$i     bb$i:/etc/sysconfig/network-scripts/ifcfg-em4
+
+    scp ifcfg-em1         bb$i:/etc/sysconfig/network-scripts/ifcfg-em1
+    scp ifcfg-em2         bb$i:/etc/sysconfig/network-scripts/ifcfg-em2
+    scp zebra.conf        bb$i:/etc/quagga/zebra.conf
+    scp bonding.conf      bb$i:/etc/modprobe.d/bonding.conf
+    scp sysctl.conf       bb$i:/etc/sysctl.conf
+    scp hosts             bb$i:/etc/hosts
+    scp rc.local          bb$i:/etc/rc.local
+    scp nginx.conf        bb$i:/etc/nginx
+    scp dstat.sh          b$i:bin/
 done
 exit
 
 
 for i in $(seq 1 6); do
-    scp r1-r6-ifcfg-lo0   rr$i:/etc/sysconfig/network-scripts/ifcfg-lo0
-    scp r1-r6.local.conf  rr$i:/etc/rsyslog.d/
+    scp r1-r6-ifcfg-lo0   bb$i:/etc/sysconfig/network-scripts/ifcfg-lo0
+    scp r1-r6.local.conf  bb$i:/etc/rsyslog.d/
 done
-scp r7.hub.conf rr7:/etc/rsyslog.d/hub.conf
+scp r7.hub.conf bb7:/etc/rsyslog.d/hub.conf
 scp clean_log_tail.sh r7:log/
 exit
 
