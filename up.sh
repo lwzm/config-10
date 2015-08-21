e7246bcd5994082f3df0a0fc5d71cecb58163df2
felloak <felloak@gmail.com>
2015-08-21 18:06:04 +0800

    r2

--- up.sh
+++ up.sh
@@ -1,30 +1,33 @@
 #!/bin/sh
 
-for i in 1 3 4 5 6; do
-    scp r1-r6.local.conf  bb$i:/etc/rsyslog.d/
+for i in 1 2 3 4 5 6; do
+    scp nginx-default.conf  bb$i:/etc/nginx/conf.d/default.conf
+    scp nginx-proxy.conf    bb$i:/etc/nginx/conf.d/proxy.conf
 done
 exit
 
-for i in 1 3 4 5 6 7 9 10; do
+for i in 1 2 3 4 5 6 7 8 9 10; do
     scp hosts             bb$i:/etc/hosts
 done
 exit
 
 for i in $(seq 1 10); do
-    scp ifcfg-bond0/r$i   bb$i:/etc/sysconfig/network-scripts/ifcfg-bond0
-    scp ospfd.conf/r$i    bb$i:/etc/quagga/ospfd.conf
-    scp ifcfg-em4/r$i     bb$i:/etc/sysconfig/network-scripts/ifcfg-em4
+    scp ifcfg-bond0/r$i     bb$i:/etc/sysconfig/network-scripts/ifcfg-bond0
+    scp ospfd.conf/r$i      bb$i:/etc/quagga/ospfd.conf
+    scp ifcfg-em4/r$i       bb$i:/etc/sysconfig/network-scripts/ifcfg-em4
 
-    scp ifcfg-em1         bb$i:/etc/sysconfig/network-scripts/ifcfg-em1
-    scp ifcfg-em2         bb$i:/etc/sysconfig/network-scripts/ifcfg-em2
-    scp zebra.conf        bb$i:/etc/quagga/zebra.conf
-    scp bonding.conf      bb$i:/etc/modprobe.d/bonding.conf
-    scp sysctl.conf       bb$i:/etc/sysctl.conf
-    scp sshd_config       bb$i:/etc/ssh/sshd_config
-    scp hosts             bb$i:/etc/hosts
-    scp rc.local          bb$i:/etc/rc.local
-    scp nginx.conf        bb$i:/etc/nginx
-    scp dstat.sh          b$i:bin/
+    scp ifcfg-em1           bb$i:/etc/sysconfig/network-scripts/ifcfg-em1
+    scp ifcfg-em2           bb$i:/etc/sysconfig/network-scripts/ifcfg-em2
+    scp zebra.conf          bb$i:/etc/quagga/zebra.conf
+    scp bonding.conf        bb$i:/etc/modprobe.d/bonding.conf
+    scp sysctl.conf         bb$i:/etc/sysctl.conf
+    scp sshd_config         bb$i:/etc/ssh/sshd_config
+    scp hosts               bb$i:/etc/hosts
+    scp rc.local            bb$i:/etc/rc.local
+    scp nginx.conf          bb$i:/etc/nginx
+    scp nginx-default.conf  bb$i:/etc/nginx/conf.d/default.conf
+    scp nginx-proxy.conf    bb$i:/etc/nginx/conf.d/proxy.conf
+    scp dstat.sh            b$i:bin/
 done
 exit
 
@@ -42,3 +45,7 @@ chmod 755 .
 ln -s /home/q/.dstat /usr/share/nginx/html/dstat
 ln -s /home/q/log /usr/share/nginx/html/log
 exit
+
+crontab <<EOF
+0 * * * * ./bin/dstat.sh >/dev/null
+EOF
